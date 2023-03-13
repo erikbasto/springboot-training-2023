efbcb2b8dd1c98fd27095016b60ce9ce5023762c
erikbasto <30513989+erikbasto@users.noreply.github.com>
2023-03-13 15:47:24 -0600

    Initial commit

---
+++ Api/recipes/src/test/java/com/ebasto/springtraining/recipes/recipes/RecipesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebasto.springtraining.recipes.recipes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
